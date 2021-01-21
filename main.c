6ceb96beb49300c2490e84f3c75f6f42d012c3b3
SupersuhiMega <SupersushiMega@example.com>
2021-01-21 15:57:35 +0100

    almost finished

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
  * Timer 1
  * UART
  * */
+ 
+ //Header
+ //==============================================
+ //Name: Umdrehungszaehler
+ //uP: ATmega328
+ //Made by Sascha Rutz
+ //==============================================
+ 
 #define F_CPU 8000000UL// set the CPU clock
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -13,10 +21,42 @@
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 8UL)))-1)
 
+#define SpeedMinusPin (PINC & (1<<PC0))	//define Speed- Pin
+#define SpeedPlusPin (PINC & (1<<PC1))	//define Speed+ Pin
+
+#define SpeedMinusMask (1<<PC0)	//define Speed- Mask
+#define SpeedPlusMask (1<<PC1)	//define Speed+ Mask
+
+#define FullRotationMask = (1<<PD2)
+
+#define PWM_ON (PORTC |= (1<<PC3))
+#define PWM_OFF (PORTC &= ~(1<<PC3))
+
+#define PWM_MIN 71
+
 //TIMER
 ISR (TIMER1_COMPA_vect);
 
+
+volatile uint8_t ISR_counter = 0;
+volatile uint16_t ms = 0;
+volatile uint16_t msUntilInput = 0;
+ISR (TIMER0_OVF_vect)
+{
+	ISR_counter++;
+	if(ISR_counter >= 32)
+	{
+		ms++;
+		if (msUntilInput)
+		{
+			msUntilInput--;
+		}
+		ISR_counter = 0;
+	}
+}
+
 //UART
+void uart_send_numb (uint16_t number);
 void uart_send_char(char c);
 void uart_send_string(volatile char *s);
 
@@ -52,20 +92,104 @@ int main(void)
 	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);	//8-Bit Char size
 	UBRR0H = (BAUD_PRESCALE >> 8);	//load upper 8-Bits of baud rate value into high byte of UBRR0H
 	UBRR0L = BAUD_PRESCALE;			//load lower 8-Bits of Baud rate into low byte of UBRR0L
-
+	
+	//Konfiguration Timer Overflow
+	//==================================================================
+	TCCR0A	= 0x00;
+	TCCR0B	= 0x01;
+	TIMSK0	|= (1 << TOIE0);
+	TIFR0 |= (1 << TOV0);
+	//==================================================================
+	
     sei();
     // enable interrupts
     
+    DDRD &= ~(1<<PD2);	//Set PD2 as Input
     
-  
+    DDRC &= ~(1<<PC0) | ~(1<<PC1);	//Set PC0 and PC1 as Inputs
+    
+    DDRC |= (1<<PC3);	//Set PC3 as Output
+    
+    uint8_t cycleCounter = 0;
+    uint8_t PWMcounter = 0;
+    uint8_t PWMedge = PWM_MIN;
+    
+    uint8_t Input = 0;
+    uint8_t LastInput = 0;
+    
+    uint8_t FullRotationLast = 0;
+    uint8_t FullRotation = 0;
+    
+    uint8_t RPScounter = 0;
+    uint16_t RPM = 0;
+    
 	while(1)
-	{ 	
-		//uart_send_char('A');
-		//uart_send_char('\n');
-		//uart_send_string("This is typo dummy text. You can see from it whether all the letters are there and what they look like. Sometimes words like hamburg fonts, rafgenduks or handgloves are used to test fonts. Sometimes sentences that contain all the letters of the alphabet - these sentences are called This one is very well known: The quick brown fox jumps over the lazy old dog. Often, foreign language parts of sentences are built into typo blind texts (AVAIL and Wefox are testing also kerning) in order to test the effect in other languages. For example, almost any font looks good in Latin. Quod erat demonstrandum. Since 1975 the numbers have been missing in most test texts, which is why according to TypoGb. 204 § from the year 2034 numbers in 86 of the texts become mandatory. Failure to comply will result in a penalty of up to 245 or 368. In the meantime, are just as important, but they are almost always included in newer scripts. OpenType functionalities are an important but difficult to integrate field. Depending on the software and preferences, built-in small caps, kerning or ligatures (very smart) may not be displayed correctly. You can see from it whether all the letters are there and what they look like. Sometimes you use words like Hamburgefonts, Rafgenduks\n\r");
-		uart_send_numb(4000);
-		_delay_ms(500);
+	{ 
+		Input = PINC;
+		FullRotation = (PIND & (1<<PD2));
+		cycleCounter++;
+		if ((cycleCounter % 8) == 0)
+		{
+			PWMcounter++;
+		}
+		if(!msUntilInput)
+		{
+			if((Input & SpeedMinusMask) & ~(LastInput & SpeedMinusMask))
+			{
+				if(PWMedge > PWM_MIN)
+				{
+					PWMedge -= 10;
+				}
+				else
+				{
+					PWMedge = 0;
+				}
+				msUntilInput = 10;
+			}
+			else if((Input & SpeedPlusMask) & ~(LastInput & SpeedPlusMask))
+			{
+				if((PWMedge + 10) < 256)
+				{
+					if(PWMedge < PWM_MIN)
+					{
+						PWMedge = PWM_MIN;
+					}
+					else
+					{
+						PWMedge += 10;
+					}
+				}
+				msUntilInput = 10;
+			}
+			LastInput = Input;
+		}
+		
+		if(!FullRotation && FullRotationLast)
+		{
+			RPScounter++;
+		}
+		
+		FullRotationLast = FullRotation;
+		
+		if(ms >= 1000)
+		{
+			RPM = RPScounter * 60;
+			uart_send_numb(RPM);
+			uart_send_char('RPM \n');
+			RPScounter = 0;
+			ms = 0;
+		}
+		
+		if(PWMcounter <= PWMedge)
+		{
+			PWM_ON;
+		}
+		else
+		{
+			PWM_OFF;
+		}
 	} //end while
+	return 0;
 }//end of main
 
 
